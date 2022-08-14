6d034ac4d5ecf50ae1b9282676d9caa17a55ec12
StreamQeen3 <104708262+StreamQeen3@users.noreply.github.com>
2022-08-14 22:31:59 +0530

    Update team.js

--- team.js
+++ team.js
@@ -2036,7 +2036,7 @@ break
                 let teks = 'YouTube Search\n\n Result From '+text+'\n\n'
                 let no = 1
                 for (let i of search.all) {
-                    teks += `❮🫣❯ ➠ No : ${no++}\n❮🫣❯ ➠ Type : ${i.type}\n❮🫣❯ ➠ Video ID : ${i.videoId}\n❮🫣❯ ➠ Title : ${i.title}\n❮🫣❯ ➠ Views : ${i.views}\n❮🫣❯ ➠ Duration : ${i.timestamp}\n❮🫣❯ ➠ Uploaded On : ${i.ago}\n❮🫣❯ ➠ Author : ${i.author.name}\n❮🫣❯ ➠ Url : ${i.url}\n\n─────────────────\n\n${botname}`
+                    teks += `❮🫣❯ ➠ No : ${no++}\n❮🫣❯ ➠ Type : ${i.type}\n❮🫣❯ ➠ Video ID : ${i.videoId}\n❮🫣❯ ➠ Title : ${i.title}\n❮🫣❯ ➠ Views : ${i.views}\n❮🫣❯ ➠ Duration : ${i.timestamp}\n❮🫣❯ ➠ Uploaded On : ${i.ago}\n❮🫣❯ ➠ Author : ${i.author.name}\n❮🫣❯ ➠ Url : ${i.url}\n\n─────────────────\n\n${botname}\n`
                 }
            RedDragonMdNx.sendMessage(m.chat, { image: { url: search.all[0].thumbnail },  caption: teks }, { quoted: m })
             }
